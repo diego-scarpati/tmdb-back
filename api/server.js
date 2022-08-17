002031067219da51719ae45e812e8e069c4d51ca
Diego Scarpati <diegoscarpati13@gmail.com>
2022-08-17 19:56:43 -0300

    changed cors to *

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", CORS_URL);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
