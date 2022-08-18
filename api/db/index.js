b4cdc1a2e5c0be763f4b1f18e367c13f5cdba789
Diego Scarpati <diegoscarpati13@gmail.com>
2022-08-18 09:52:29 -0300

    removed user and password from db config

--- api/db/index.js
+++ api/db/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const { DATABASE_URL, USER, PASSWORD } = process.env;
 
-const db = new Sequelize(DATABASE_URL, USER, PASSWORD, {
+const db = new Sequelize(DATABASE_URL, {
   logging: false,
   native: false,
   dialect: "postgres",
